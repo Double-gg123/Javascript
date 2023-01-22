27db9035769ca0a23a495ecbcab5a2a3daaf0988
Double-gg123 <71548390+Double-gg123@users.noreply.github.com>
2023-01-22 22:08:11 +0200

    Add files via upload

---
+++ sorting js.js
@@ -0,0 +1,41 @@
+/*const numbers =[33,2,8];
+numbers.sort();
+console.log(numbers[1])
+with sorting it considers which one comes first may it be numbers or letters
+*/
+//console.log("Helloworld");
+// var x=5;
+// console.log(x);
+// Operations
+// var a =4;
+
+// var b =6;
+// var c= a-b
+// console.log(c);
+//   var a=8;
+//   a--
+//   console.log(a);
+// Arrays
+// var myArray=new Array("greg","John","Peter");
+// console.log(myArray[0]);
+// or 
+// var myArray= ["Greg","John","Peter"];
+// console.log(myArray[0]);
+// var names= ["Greg","Aohn","Peter"];
+// names.sort();
+// console.log(names[0]);
+// As for names in sort it considers what comes firts alphabetically
+// Function 
+// function greet(person){
+//     console.log("Hii, "+ person);
+// }
+// greet ("Max");
+// greet ("Stacey");
+// greet ("Jay");
+// greet ("Kelly");
+// For loop
+// for (i=0; i<=3; i++){
+//     console.log("Hi");
+// }
+// it appears on the browser
+// document .write("Hello");
